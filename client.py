51a4cf7e5aee9cfb789236dd9fe4a5e962e3e667
Amir <amirhossein.d.z@gmail.com>
2025-08-09 19:21:14 +0200

    add process_query method

--- client.py
+++ client.py
@@ -50,3 +50,76 @@ class MCPClient:
         tools = response.tools
 
         print("Connected to MCP server with tools:", [tool.name for tool in tools])
+
+    async def process_query(self, query: str) -> str:
+        """Process a query using Claude and available tools"""
+
+        messages = [
+            {
+                "role": "user",
+                "content": query
+            }
+        ]
+
+        response = await self.session.list_tools()
+        available_tools = [
+            {
+                "name": tool.name,
+                "description": tool.description,
+                "input_schema": tool.inputSchema
+            } for tool in response.tools
+        ]
+
+        # initial call to Claude
+        response = await self.antropic.messages.create(
+            model="claude-3-5-sonnet-20241022",
+            messages=messages,
+            tools=available_tools,
+            max_tokens=1000
+        )
+
+        # process response and check for tool calls
+        final_text = []
+
+        assistant_message_content = []
+        for content in response.content:
+            if content.type == "text":
+                final_text.append(content.text)
+                assistant_message_content.append(content)
+            elif content.type == "tool_use":
+                tool_name = content.name
+                tool_args = content.input
+
+                result = await self.session.call_tool(
+                    tool_name=tool_name,
+                    tool_args=tool_args
+                )
+                final_text.append(f"[Calling tool {tool_name} with args {tool_args}]")
+
+                assistant_message_content.append(content)
+                messages.append({
+                    "role": "assistant",
+                    "content": assistant_message_content
+                })
+                messages.append({
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "tool_result",
+                            "tool_use_id": content.id,
+                            content: result.content
+                        }
+                    ]
+                })
+
+                # get next response from Claude
+                response = await self.antropic.messages.create(
+                    model="claude-3-5-sonnet-20241022",
+                    messages=messages,
+                    tools=available_tools,
+                    max_tokens=1000
+                )
+
+                final_text.append(response.content[0].text)
+
+        return "\n".join(final_text)
